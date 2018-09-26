6526f3ee8d7fb0a4d29381695bcd4cd6476c757c
pebezo <peterzolja@gmail.com>
2018-09-26 16:10:55 -0400

    First

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace chris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Chris and Pete!");
+        }
+    }
+}
